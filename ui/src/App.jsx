54ab2356d2f25f9f1553cff232cba22d488d7258
Kamila Murawska <kamilaa.murawska@gmail.com>
2019-05-29 19:50:46 +0200

    add notification bell with badge

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AppBar from "@material-ui/core/es/AppBar/AppBar";
+import Toolbar from "@material-ui/core/es/Toolbar/Toolbar";
+import Typography from "@material-ui/core/es/Typography/Typography";
+import NotificationBell from "./bell/NotificationBell.jsx";
 
 export default class App extends React.Component {
     render() {
         return (
-            <div>
+            <div style={{flexGrow: 1}}>
+                <AppBar position="static" color="default">
+                    <Toolbar>
+                        <Typography variant="h6" color="inherit" style={{flexGrow: 1}}>
+                            Title
+                        </Typography>
+                        <NotificationBell/>
+                    </Toolbar>
+                </AppBar>
             </div>
         )
     }
--- ui/src/bell/NotificationBell.jsx
+++ ui/src/bell/NotificationBell.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import Notifications from "@material-ui/icons/Notifications";
+import Badge from "@material-ui/core/es/Badge/Badge";
 
 export default class NotificationBell extends React.Component {
-    constructor() {
-        super();
+    render() {
+        return (
+            <a style={{cursor: "pointer"}} className="no-right-padding" onClick={this.openNotifications}>
+                <Badge badgeContent={4} color="primary">
+                    <Notifications/>
+                </Badge>
+            </a>
+        )
     }
 
-    render() {
-        return <li style={{position: "static"}}>
-            <a style={{cursor: "pointer"}}>
-                <div></div>
-            </a>
-        </li>
+    openNotifications() {
+        console.log("A")
     }
 }
